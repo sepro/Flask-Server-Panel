adf89d98876d3aaf52e5e0eb13453393e35c4398
Sebastian Proost <sebastian.proost@gmail.com>
2016-01-18 09:52:57 +0100

    styline pi-hole stats

--- serverpanel/static/jsx/pihole.jsx
+++ serverpanel/static/jsx/pihole.jsx
@@ -23,17 +23,21 @@
             if (this.state.data.enabled) {
               return (<div><h3>Pi-Hole</h3>
               <div className="row">
-                 <div className="col-lg-3 col-xs-6">
-                 <p>DNS queries today: { this.state.data.dns_queries_today }</p>
+                 <div className="col-lg-3 col-xs-6 text-center">
+                 <h4>{ this.state.data.dns_queries_today }</h4>
+                 <p>DNS queries today</p>
                  </div>
-                 <div className="col-lg-3 col-xs-6">
-                 <p>Ads blocked today: { this.state.data.ads_blocked_today }</p>
+                 <div className="col-lg-3 col-xs-6  text-center">
+                 <h4>{ this.state.data.ads_blocked_today }</h4>
+                 <p>Ads blocked today</p>
                  </div>
-                 <div className="col-lg-3 col-xs-6">
-                 <p>Percentage blocked: { this.state.data.ads_percentage_today.toFixed(2) } %</p>
+                 <div className="col-lg-3 col-xs-6  text-center">
+                 <h4>{ this.state.data.ads_percentage_today.toFixed(2) } %</h4>
+                 <p>Percentage blocked</p>
                  </div>
-                 <div className="col-lg-3 col-xs-6">
-                 <p>Blocked domains: { this.state.data.blocked_domains }</p>
+                 <div className="col-lg-3 col-xs-6  text-center">
+                 <h4>{ this.state.data.blocked_domains }</h4>
+                 <p>Blocked domains</p>
                  </div>
               </div>
                  </div>);
