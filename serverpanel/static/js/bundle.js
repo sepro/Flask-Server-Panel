4fbba94ce48ae4e10a59125a34d598abbe97dde7
sepro <sebastian.proost@gmail.com>
2016-02-24 15:59:11 +0100

    added pihole

--- serverpanel/static/js/bundle.js
+++ serverpanel/static/js/bundle.js
@@ -70,6 +70,10 @@
 
 	var _processes2 = _interopRequireDefault(_processes);
 
+	var _pihole = __webpack_require__(167);
+
+	var _pihole2 = _interopRequireDefault(_pihole);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 /***/ },
@@ -20263,6 +20267,128 @@
 	}(_timed_component2.default);
 
 	_reactDom2.default.render(_react2.default.createElement(Processes, { url: document.getElementById('processes').getAttribute('url'), pollInterval: 5000 }), document.getElementById('processes'));
+
+/***/ },
+/* 167 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _timed_component = __webpack_require__(161);
+
+	var _timed_component2 = _interopRequireDefault(_timed_component);
+
+	var _react = __webpack_require__(2);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactDom = __webpack_require__(159);
+
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Pihole = function (_TimedComponent) {
+	  _inherits(Pihole, _TimedComponent);
+
+	  function Pihole() {
+	    _classCallCheck(this, Pihole);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Pihole).apply(this, arguments));
+	  }
+
+	  _createClass(Pihole, [{
+	    key: 'render',
+	    value: function render() {
+	      if (this.state.data.enabled) {
+	        return _react2.default.createElement(
+	          'div',
+	          null,
+	          _react2.default.createElement(
+	            'h3',
+	            null,
+	            'Pi-Hole'
+	          ),
+	          _react2.default.createElement(
+	            'div',
+	            { className: 'row' },
+	            _react2.default.createElement(
+	              'div',
+	              { className: 'col-lg-3 col-xs-6 text-center' },
+	              _react2.default.createElement(
+	                'h4',
+	                null,
+	                this.state.data.dns_queries_today
+	              ),
+	              _react2.default.createElement(
+	                'p',
+	                null,
+	                'DNS queries today'
+	              )
+	            ),
+	            _react2.default.createElement(
+	              'div',
+	              { className: 'col-lg-3 col-xs-6  text-center' },
+	              _react2.default.createElement(
+	                'h4',
+	                null,
+	                this.state.data.ads_blocked_today
+	              ),
+	              _react2.default.createElement(
+	                'p',
+	                null,
+	                'Ads blocked today'
+	              )
+	            ),
+	            _react2.default.createElement(
+	              'div',
+	              { className: 'col-lg-3 col-xs-6  text-center' },
+	              _react2.default.createElement(
+	                'h4',
+	                null,
+	                this.state.data.ads_percentage_today.toFixed(2),
+	                ' %'
+	              ),
+	              _react2.default.createElement(
+	                'p',
+	                null,
+	                'Percentage blocked'
+	              )
+	            ),
+	            _react2.default.createElement(
+	              'div',
+	              { className: 'col-lg-3 col-xs-6  text-center' },
+	              _react2.default.createElement(
+	                'h4',
+	                null,
+	                this.state.data.blocked_domains
+	              ),
+	              _react2.default.createElement(
+	                'p',
+	                null,
+	                'Domains blocked'
+	              )
+	            )
+	          )
+	        );
+	      } else {
+	        return _react2.default.createElement('div', null);
+	      }
+	    }
+	  }]);
+
+	  return Pihole;
+	}(_timed_component2.default);
+
+	_reactDom2.default.render(_react2.default.createElement(Pihole, { url: document.getElementById('pihole').getAttribute('url'), pollInterval: 10000 }), document.getElementById('pihole'));
 
 /***/ }
 /******/ ]);
--- serverpanel/static/js/main.js
+++ serverpanel/static/js/main.js
@@ -4,3 +4,4 @@ import Memory from '../jsx/memory.jsx';
 import Swap from '../jsx/swap.jsx';
 import Disk from '../jsx/disk.jsx';
 import Processes from '../jsx/processes.jsx';
+import Pihole from '../jsx/pihole.jsx';
